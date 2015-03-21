9e876736ea4acf585de4c4f7287bebcaa1de628f
Julio Faerman <jfaerman@gmail.com>
2015-03-21 11:42:58 -0300

    EC2 Example

--- S03/devops/E01-worker-userdata.sh
+++ S03/devops/E01-worker-userdata.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+curl -sL https://rpm.nodesource.com/setup | bash -
+yum -y install gcc-c++ make git nodejs
+npm install forever initd-forever -g
+git clone https://github.com/AWSHubBR/devops-aws /opt/devops-aws
+
+
