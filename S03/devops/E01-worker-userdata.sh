513ab03a3bbd32462d9063fb5fc0456d8051ecba
Julio Faerman <jfaerman@gmail.com>
2015-03-21 12:07:38 -0300

    Better init.d

--- S03/devops/E01-worker-userdata.sh
+++ S03/devops/E01-worker-userdata.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Setup
+useradd -m -d /home/node node
 curl -sL https://rpm.nodesource.com/setup | bash -
 yum -y install gcc-c++ make git nodejs
 npm install forever initd-forever -g
-useradd -m -d /home/node node
 
+# Setup devops_app
 git clone https://github.com/AWSHubBR/devops-aws /opt/devops-aws
+npm install -g /opt/devops-aws/devops_app
 ln -s /opt/devops-aws/devops_app/init/devops_app /etc/init.d/devops_app
 chmod +x /etc/init.d/devops_app
 
