ed3bf235d1d4caa8f45840227f2729bfbcfa3378
Julio Faerman <jfaerman@gmail.com>
2015-03-21 11:50:36 -0300

    fix

--- S03/devops/E01-worker-userdata.sh
+++ S03/devops/E01-worker-userdata.sh
@@ -3,6 +3,9 @@
 curl -sL https://rpm.nodesource.com/setup | bash -
 yum -y install gcc-c++ make git nodejs
 npm install forever initd-forever -g
-git clone https://github.com/AWSHubBR/devops-aws /opt/devops-aws
+useradd -m -d /home/node node
 
+git clone https://github.com/AWSHubBR/devops-aws /opt/devops-aws
+ln -s /opt/devops-aws/devops_app/init/devops_app /etc/init.d/devops_app
+chmod +x /etc/init.d/devops_app
 
