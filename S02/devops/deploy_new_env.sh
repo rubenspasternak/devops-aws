1549d32d5c89718ba18ab5d0c0f66bd3cea09248
EC2 Default User <ec2-user@ip-172-31-44-35.us-west-2.compute.internal>
2015-03-23 17:38:09 +0000

    hotfix

--- S02/devops/deploy_new_env.sh
+++ S02/devops/deploy_new_env.sh
@@ -16,7 +16,7 @@ ENV_BUCKET="s3://$ENV_ID"
 echo "Creating Bucket"
 aws s3 mb $ENV_BUCKET
 
-ENV_STATICS=$ENV_DIR/devops-aws/devops-app/public/
+ENV_STATICS=$ENV_DIR/devops-aws/devops_app/public/
 echo "[$ENV_STATICS]=>[$ENV_BUCKET]"
 aws s3 sync $ENV_STATICS $ENV_BUCKET
 
